930ce54d49dff65790fb62213a767911ce24949b
LarsSeverson <larsseverson1@gmail.com>
2023-05-10 18:42:34 -0700

    refactor

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -35,7 +35,6 @@ def minimax(position, depth, max_player, game, draw_moves):
             max_eval = max(max_eval, evaluation)
             if max_eval == evaluation:
                 best_move = move
-        white += 1
         return max_eval, best_move
     else:
         min_eval = float('inf')
